987a7ad5245fd1e202152913177deb50abad7f5f
Rodrigo Edson Fernandes <rodrigoedson@gmail.com>
2022-07-25 06:14:31 -0300

    Third day

--- src/Start.java
+++ src/Start.java
@@ -1,40 +1,25 @@
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
-import java.util.Map;
 
 public class Start {
     public static void main(String[] args) throws IOException {
-        var url = "https://alura-imdb-api.herokuapp.com/movies";
-        var uri = URI.create(url);
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(uri).GET().build();
+        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-07-10&end_date=2022-07-20";
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
 
-        String body;
-        try {
-            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-            body = response.body();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        var clienteHttp = new ClienteHttp();
+        String json = clienteHttp.buscaDados(url);
 
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
 
         var geradora = new GeradoraDeFigurinhas();
         InputStream inputStream;
-        for (Map<String, String> filme : listaDeFilmes) {
-            inputStream = new URL(filme.get("image")).openStream();
-            geradora.cria(inputStream, filme.get("title"));
-            System.out.println(filme.get("title"));
+        for (Conteudo conteudo : conteudos) {
+            inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            geradora.cria(inputStream, conteudo.getTitulo());
+            System.out.println(conteudo.getTitulo());
         }
     }
 }
