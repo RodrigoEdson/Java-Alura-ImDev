8a8ff6cec4c1850733d56a5dd94406271d332188
Rodrigo Edson Fernandes <rodrigoedson@gmail.com>
2022-07-25 21:56:31 -0300

    Fourth day

--- src/Start.java
+++ src/Start.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 public class Start {
     public static void main(String[] args) throws IOException {
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-07-10&end_date=2022-07-20";
-        ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+        //var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-07-10&end_date=2022-07-20";
+        //ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+
+        var url = "http://localhost:8080/linguagens";
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
 
         var clienteHttp = new ClienteHttp();
         String json = clienteHttp.buscaDados(url);
